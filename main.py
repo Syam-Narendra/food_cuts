89d4bcc668c4afbef4a66f4d5ff46a8afb46cf56
Syam <syamnarendra85@gmail.com>
2025-03-21 13:04:19 +0530

    playwright instagram reels infinite crawlling and saving reels link

--- main.py
+++ main.py
@@ -1,43 +1,47 @@
-import asyncio
-from crawlee.crawlers import PlaywrightCrawler, PlaywrightCrawlingContext
-from playwright.async_api import Error as PlaywrightError
-from cookies import cookies
+import json
 import time
-async def request_handler(context: PlaywrightCrawlingContext) -> None:
-    try:
-        context.log.info(f"Processing URL: {context.request.url}")
-        print(cookies)
-        await context.page.context.add_cookies(cookies)
-        initial_url = context.page.url
-        await context.push_data({"link": initial_url})
-        context.log.info(f"Initial Reel URL: {initial_url}")
+from playwright.sync_api import sync_playwright
+from playwright_stealth import stealth
+from cookies import cookies
 
-        # Press "PageDown" to move to the next reel
-        time.sleep(5000)
-        await context.page.keyboard.press("PageDown")
-        print("PageDown pressed")
-        time.sleep(10000)  # import time
 
-        # Wait for the URL to change
+def open_instagram():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(
+            headless=True,
+            args=[
+                "--disable-blink-features=AutomationControlled",
+                "--disable-features=IsolateOrigins,site-per-process",
+            ],
+        )
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+        )
+        context.add_cookies(cookies)
+        page = context.new_page()
+        page.goto("https://www.instagram.com/reels/DHYngWzTNld", wait_until="load")
+        page.wait_for_timeout(5000)
+        print("Scrolling started... Press CTRL+C to stop.")
+        last_url = page.url
+        data = []
+        jsonFilePath = "reelsLinksData.json"
+        while True:
+            page.keyboard.press("PageDown")
+            time.sleep(2)
+            new_url = page.url
+            if new_url != last_url:
+                print("New Reel URL:", new_url)
+                with open(jsonFilePath, "r") as file:
+                    try:
+                        oldFileData = json.load(file)
+                        data = oldFileData
+                    except:
+                        data = []
+                with open(jsonFilePath, "w") as file:
+                    newUrl = {"link": new_url}
+                    data.append(newUrl)
+                    json.dump(data, file, indent=4)
+                last_url = new_url
 
-        # Capture the new URL
-        new_url = context.page.url
-        await context.push_data({"link": new_url})
-        context.log.info(f"New Reel URL: {new_url}")
 
-    except PlaywrightError as e:
-        context.log.error(f"Playwright error on {context.request.url}: {e}")
-    except Exception as e:
-        context.log.error(f"Unexpected error on {context.request.url}: {e}")
-
-async def main():
-    crawler = PlaywrightCrawler(headless=False)
-    crawler.router.default_handler(request_handler)
-
-    try:
-        await crawler.run(["https://www.instagram.com/reels/DA8WnYvyrl9/"])
-    except Exception as e:
-        print(f"Failed to run crawler: {e}")
-
-if __name__ == "__main__":
-    asyncio.run(main())
+open_instagram()
